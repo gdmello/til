dccbd2ec6e76727dd1ec9dc4cecd19962a383db8
Kevin Qiu <kevin@freshbooks.com>
2015-04-30 12:48:27 -0400

    cli subpackage

--- cli/main.go
+++ cli/main.go
@@ -1,5 +1,25 @@
 package cli
 
-func Run() int {
-	return 0
+import (
+	"log"
+	_ "os"
+
+	"github.com/mitchellh/cli"
+)
+
+func Run(args []string) int {
+	c := cli.NewCLI("til", Version)
+	c.Args = args
+
+	// c.Commands = map[string]cli.CommandFactory{
+	//     "foo": fooCommandFactory,
+	//     "bar": barCommandFactory,
+	// }
+
+	exitStatus, err := c.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	return exitStatus
 }
