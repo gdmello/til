cb70483b16c49b1fc06838a847acfdec74757ad7
Kevin Qiu <kevin@freshbooks.com>
2015-04-30 14:37:20 -0400

    Add missing methods

--- command/main.go
+++ command/main.go
@@ -6,3 +6,11 @@ type InitCommand struct {
 func (c *InitCommand) Run(args []string) int {
 	return 0
 }
+
+func (c *InitCommand) Help() string {
+	return "Initialize the TIL repository, default to the current directory."
+}
+
+func (c *InitCommand) Synopsis() string {
+	return "Initialize the TIL repository"
+}
