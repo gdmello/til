59eabb161e0b6b11c18035a27a5394155536d9ab
Kevin Qiu <kevin@freshbooks.com>
2015-05-02 13:34:03 -0400

    Add a mock library

--- cli/gencfg_test.go
+++ cli/gencfg_test.go
@@ -3,6 +3,7 @@ package cli_test
 import (
 	"github.com/codegangsta/cli"
 	. "github.com/kevinjqiu/til/cli"
+	_ "github.com/stretchr/testify/mock"
 
 	. "github.com/onsi/ginkgo"
 	// . "github.com/onsi/gomega"
